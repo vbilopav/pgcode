6e21682af88b56615eb2607526b6ccb90ff6c8cf
Vedran Bilopavlović <vbilopav@gmail.com>
2019-11-06 15:38:21 +0100

    initial config

---
+++ dev/index.js
@@ -0,0 +1,14 @@
+///<reference path="../node_modules/ihjs/build/1.2.6/types/core.d.ts"/>
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    class default_1 {
+        render() {
+            return String.html `
+        <div>hello</div>
+        `;
+        }
+    }
+    exports.default = default_1;
+});
+//# sourceMappingURL=index.js.map
---
+++ dev/index.ts
@@ -0,0 +1,9 @@
+///<reference path="../node_modules/ihjs/build/1.2.6/types/core.d.ts"/>
+
+export default class {
+    render() {
+        return String.html`
+        <div>hello</div>
+        `;
+    }
+}
